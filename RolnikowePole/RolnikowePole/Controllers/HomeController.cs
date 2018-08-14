cf6dd35b169b0be3d28b92f8c4147b3bdbd5d48b
Jakub47 <jakub7447@gmail.com>
2018-08-14 23:39:16 +0200

    Baza Danych + Context + 1 rekord

---
+++ RolnikowePole/RolnikowePole/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using RolnikowePole.DAL;
+using RolnikowePole.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RolnikowePole.Controllers
+{
+    public class HomeController : Controller
+    {
+        RolnikowePoleContext db = new RolnikowePoleContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var gatunek = new Gatunek() { NazwaGatunku = "Ryby", OpisGatunku = "Test" };
+            db.Gatunki.Add(gatunek);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ RolnikowePole/RolnikowePole/DAL/RolnikowePoleContext.cs
@@ -0,0 +1,29 @@
+﻿using RolnikowePole.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace RolnikowePole.DAL
+{
+    public class RolnikowePoleContext : DbContext
+    {
+        public RolnikowePoleContext() : base("RolnikowePoleContext")
+        {
+
+        }
+
+        public DbSet<Zwierze> Zwierzeta { get; set; }
+        public DbSet<Gatunek> Gatunki { get; set; }
+        public DbSet<Zamowienie> Zamowienia { get; set; }
+        public DbSet<PozycjaZamowienia> PozycjeZamowienia { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
