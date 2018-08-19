2ddf73c536355d9967395a151374e4d6e9165a1e
Jakub47 <jakub7447@gmail.com>
2018-08-19 19:28:56 +0200

    Dynamiczna list i obrazy w widoku (3/4)

---
+++ RolnikowePole/RolnikowePole/Infrastucture/AppConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace RolnikowePole.Infrastucture
+{
+    public class AppConfig
+    {
+        private static string _IkonyGatunkowFolderWzgledny = ConfigurationManager.AppSettings["IkonyGatunkowFolder"];
+
+        public static string IkonyGatunkowFolderWzgledny
+        {
+            get
+            {
+                return _IkonyGatunkowFolderWzgledny;
+            }
+        }
+
+        private static string _obrazkiFolderWzgledny = ConfigurationManager.AppSettings["ObrazkiFolder"];
+
+        public static string ObrazkiFolderWzgledny
+        {
+            get
+            {
+                return _obrazkiFolderWzgledny;
+            }
+        }
+    }
+}
---
+++ RolnikowePole/RolnikowePole/Infrastucture/UrlHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RolnikowePole.Infrastucture
+{
+    public static class UrlHelpers
+    {
+        public static string IkonyGatunkowSciezka(this UrlHelper helper, string nazwaIkonyGatunku)
+        {
+            var IkonyGatunkiFolder = AppConfig.IkonyGatunkowFolderWzgledny;
+            var sciezka = Path.Combine(IkonyGatunkiFolder, nazwaIkonyGatunku);
+            var sciezkaBezWzgledna = helper.Content(sciezka);
+
+            return sciezkaBezWzgledna;
+        }
+
+        public static string ObrazkiSciezka(this UrlHelper helper, string nazwaObrazka)
+        {
+            var ObrazkiFolder = AppConfig.ObrazkiFolderWzgledny;
+            var sciezka = Path.Combine(ObrazkiFolder, nazwaObrazka);
+            var sciezkaBezWzgledna = helper.Content(sciezka);
+
+            return sciezkaBezWzgledna;
+        }
+    }
+}
