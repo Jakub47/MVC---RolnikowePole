3caee6612d840da0ad4734d5a0bfb038acfce78e
Jakub47 <jakub7447@gmail.com>
2018-08-26 17:18:51 +0200

    Filtrowanie działa poprawnie zastanowić się nad jego praktycznym zastosowaniem w apliklacji !

--- RolnikowePole/RolnikowePole/Global.asax.cs
+++ RolnikowePole/RolnikowePole/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using RolnikowePole.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace RolnikowePole
@@ -13,6 +15,7 @@ namespace RolnikowePole
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
