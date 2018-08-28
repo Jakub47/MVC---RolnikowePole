fcf621e9d963606d0ff5b679fd8109751509b17a
Jakub47 <jakub7447@gmail.com>
2018-08-28 07:34:33 +0200

    Implementacja widoku koszyka

---
+++ RolnikowePole/RolnikowePole/ViewModels/KoszykViewModel.cs
@@ -0,0 +1,14 @@
+﻿using RolnikowePole.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RolnikowePole.ViewModels
+{
+    public class KoszykViewModel
+    {
+        public List<Zwierze> PozycjeKoszyka { get; set; }
+        public decimal CenaCalkowita { get; set; }
+    }
+}
