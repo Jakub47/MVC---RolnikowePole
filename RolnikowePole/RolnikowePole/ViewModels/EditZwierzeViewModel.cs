8314fcc88571712040320b830eb6366e270a1608
Jakub47 <jakub7447@gmail.com>
2018-09-03 12:37:15 +0200

    1)Dodawanie i Edycja Zwierze Tylko przez Administratora 2)Stowrzenie widoku 3)Ddanie akcji do ManagerControler

---
+++ RolnikowePole/RolnikowePole/ViewModels/EditZwierzeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using RolnikowePole.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RolnikowePole.ViewModels
+{
+    public class EditZwierzeViewModel
+    {
+        public Zwierze Zwierze { get; set; }
+        public IEnumerable<Gatunek> Gatunki { get; set; }
+        public bool? Potwierdzenie { get; set; }
+    }
+}
