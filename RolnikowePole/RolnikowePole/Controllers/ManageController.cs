bf81f669e3ab61b57cccd0e0bc5e38a818bee936
Jakub47 <jakub7447@gmail.com>
2018-10-14 22:19:44 +0200

    W WIDOKU 'DODAJZWIERZE' USUNĄĆ POLE 'WYROZNIONY' I SPRAWDZIĆ ZDJĘCIE PRZED JEGO WSTAWIENIEM DO BAZY

--- RolnikowePole/RolnikowePole/Controllers/ManageController.cs
+++ RolnikowePole/RolnikowePole/Controllers/ManageController.cs
@@ -14,6 +14,9 @@ using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Data.Entity.Migrations;
 using System.Data.Entity.Validation;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -254,13 +257,18 @@ namespace RolnikowePole.Controllers
                     //Czy Pozostałe pola zostały wypełnione poprawnie
                     if (ModelState.IsValid)
                     {
+                        var sourceImage = Image.FromStream(file.InputStream);
+
+                        sourceImage = ResizeImage(sourceImage, 700, 400);
+
                         //Generowanie plik
                         var fileExt = Path.GetExtension(file.FileName);
                         var filename = Guid.NewGuid() + fileExt; // Unikalny identyfikator + rozszerzenie
 
                         //W jakim folderze ma byc umiesczony dany plik oraz jego nazwa! Oraz zapis
                         var path = Path.Combine(Server.MapPath(AppConfig.ObrazkiFolderWzgledny), filename);
-                        file.SaveAs(path);
+                        //file.SaveAs(path);
+                        sourceImage.Save(path);
 
                         model.Zwierze.NazwaPlikuObrazka = filename;
                         model.Zwierze.DataDodania = DateTime.Now;
@@ -293,6 +301,39 @@ namespace RolnikowePole.Controllers
                     return View(model);
                 }
             }
+        }
+
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        [NonAction]
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
         }
 
         public ActionResult WyswietlWiadomosciUzytkownika()
