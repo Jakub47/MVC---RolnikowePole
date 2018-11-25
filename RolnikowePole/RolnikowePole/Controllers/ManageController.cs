de94e44ba1cab09f9b0955ca74028e60226b2147
Jakub47 <jakub7447@gmail.com>
2018-11-25 22:32:55 +0100

    Logika zdjecia sprawdzic logike!

--- RolnikowePole/RolnikowePole/Controllers/ManageController.cs
+++ RolnikowePole/RolnikowePole/Controllers/ManageController.cs
@@ -236,16 +236,39 @@ namespace RolnikowePole.Controllers
         public ActionResult UsunZdjecie(int zdjecieId)
         {
             var Zdjecie = db.Zdjecie.Find(zdjecieId);
+            
+
             var ZwierzeId = Zdjecie.ZwierzeId;
+            var Zwierze = db.Zwierzeta.Find(ZwierzeId);
 
             //Sprawdz ile zdjec jest obecnie przypisanych do tego zwierza
             var Zdjecia = db.Zdjecie.Where(a => a.ZwierzeId == ZwierzeId && a.ZdjecieID == zdjecieId).ToList();
+
+            if (Zdjecia == null)
+            {
+                var zd = new Zdjecie()
+                {
+                    FilePath = "Domyslne.png",
+                    ZwierzeId = ZwierzeId
+                };
+                db.Entry(zd).State = EntityState.Deleted;
+            }
 
             //Sprawdz czy usuwane zdjecie jest glownym zdjeciem tego Zwierza
             var ZdjecieGlowne = db.Zwierzeta.Where(a => a.NazwaPlikuObrazka == Zdjecie.ZdjecieID.ToString()).SingleOrDefault();
             if(ZdjecieGlowne != null)
             {
-
+                //Sprawdz czy zwierze ma inne Zdjecia
+                if(Zdjecia != null)
+                {
+                    //Ustaw inne zdjecie głównym zdjeciem
+                    Zwierze.NazwaPlikuObrazka = Zdjecia.Take(1).FirstOrDefault().FilePath;
+                }
+                //W innym przypadku ustaw domyślne zdjecie
+                else
+                {
+                    Zwierze.NazwaPlikuObrazka = "Domyslne.png";
+                }
             }
 
             db.Entry(Zdjecie).State = EntityState.Deleted;
